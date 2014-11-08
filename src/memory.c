9fa159389a6c0014228920b078d44aa05defc2da
Roberto De Ioris <info@unbit.it>
2014-11-08 19:56:59 +0100

    do not report peer removal on client

--- src/memory.c
+++ src/memory.c
@@ -1,21 +1,23 @@
 #include "vpn-ws.h"
 
 void vpn_ws_peer_destroy(vpn_ws_peer *peer) {
-	vpn_ws_log("removing peer %X:%X:%X:%X:%X:%X (fd: %d)\n", peer->mac[0],
+	int fd = peer->fd;
+	
+	if (fd > -1) {
+		vpn_ws_log("removing peer %2.X:%2.X:%2.X:%2.X:%2.X:%2.X (fd: %d)\n", peer->mac[0],
                         peer->mac[1],
                         peer->mac[2],
                         peer->mac[3],
                         peer->mac[4],
                         peer->mac[5], peer->fd);
-	int fd = peer->fd;
+		close(fd);
+	}
+
 	if (peer->buf) free(peer->buf);
 	free(peer);
 
 	if (vpn_ws_conf.peers)
 		vpn_ws_conf.peers[fd] = NULL;
-
-	if (fd > -1)
-		close(fd);
 }
 
 void *vpn_ws_malloc(uint64_t amount) {
